f1be409a42dd65a3f63b54272b31ecaab9082e87
Oğuz Geylani Yılmaz <oguzgeylaniyilmaz@gmail.com>
2025-08-08 23:08:55 +0200

    basic steps for JWT

---
+++ RealEstate.API/Tools/JwtTokenDefault.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstate.API.Tools
+{
+    public class JwtTokenDefault
+    {
+        public const string ValidAudience = "https://localhost";
+        public const string ValidIssuer = "https://localhost";
+        public const string SecretKey = "RealEstate..12345678";
+        public const int ExpirationMinutes = 10; // Token expiration time in minutes
+    }
+}
