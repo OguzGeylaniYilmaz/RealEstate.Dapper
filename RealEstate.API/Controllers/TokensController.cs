1f45aaecb6d7423e1c6854d36787aef4c99e9960
Oğuz Geylani Yılmaz <oguzgeylaniyilmaz@gmail.com>
2025-08-10 22:46:11 +0200

    creating mvc area

---
+++ RealEstate.API/Controllers/TokensController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate.API.Tools;
+
+namespace RealEstate.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokensController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult CreateToken(GetCheckAppUserViewModel appUserViewModel)
+        {
+
+            var values = JwtTokenGenerator.GenerateToken(appUserViewModel);
+            return Ok(values);
+        }
+    }
+}
---
+++ RealEstate.UI/ViewComponents/EstateAgent/AgentSidebar.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate.UI.ViewComponents.EstateAgent
+{
+    public class AgentSidebar : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            // You can pass any model or data to the view if needed
+            return View();
+        }
+    }
+}
